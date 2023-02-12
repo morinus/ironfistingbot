b2035f683333f20201f6ce11b88092400e2ef738
Luka Mihaldinec <luka.mihaldinec@gmail.com>
2023-02-12 23:27:39 +0100

    [ADD] Added deploy-commands, added slash command handling

---
+++ deploy-commands.js
@@ -0,0 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+require("dotenv").config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles) {
+    const command = require(`./src/commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch(error) {
+        console.error(error);
+    }
+})();
