fc92c1e0c0f4cf495e05642903bc60a16e890fe6
Luka Mihaldinec <luka.mihaldinec@gmail.com>
2023-05-19 19:35:24 +0200

    [ADD] Added /help

---
+++ src/commands/help.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const commandsDatabase = require('../../databases/commands-database.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Lists the commands available to use with our bot.'),
+    async execute(interaction) {
+
+        var listOfCommandsMessage = GetListOfCommandsMessage();
+
+        await interaction.reply(listOfCommandsMessage);
+    }
+}
+
+function GetListOfCommandsMessage() {
+
+    let message = "List of commands:\n```";
+
+    Object.entries(commandsDatabase).forEach((entry) => {
+        const [key, value] = entry;
+        message += `/` + key + " - " + value + `\n`;
+    });
+    
+    message += "```";
+
+    return message;
+}
